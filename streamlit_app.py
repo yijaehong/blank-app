13949143fa76b836af6a0adddcfa010204f5487e
yijaehong <132038641+yijaehong@users.noreply.github.com>
2024-12-03 15:07:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -147,7 +147,13 @@ with st.container():
 
             df_3=df_3.reset_index()
 
+            
+            st.title('Map with Location Data')
+            st.caption(
+                "Displaying geographical data on a map using Streamlit and Folium")
+            
             st.subheader("World Map :: Streamlit map")
+            
             list_from_df = df_3['latitude'].tolist()
             list_from_df = list(map(float, list_from_df)) 
             list_from_df2 = df_3['longitude'].tolist()
@@ -204,10 +210,8 @@ with st.container():
                 
                 
             #3 지도 제목과 캡션 추가
-            st.title('Map with Location Data : Folium feat')
-            st.caption(
-                "Displaying geographical data on a map using Streamlit and Folium")
 
+            st.subheader("Map with Location Data : Folium feat")
             #4 지도 시각화
             st.components.v1.html(my_map._repr_html_(), width=800, height=600)
 
